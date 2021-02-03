58a0e992b1a458ead2d25ad07cf96ac98cd76dfa
VergilZh <z1205067147@gmail.com>
2021-02-02 21:43:17 -0500

    Add bullet and test player

---
+++ Assets/3_Scripts/Player/BulletBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour
+{
+    public float bulletSpeed = 30;
+    public float lifeTime = 3;
+
+    private void Start()
+    {
+        Vector3 rotation = transform.rotation.eulerAngles;
+
+        transform.rotation = Quaternion.Euler(rotation.x, transform.eulerAngles.y, rotation.z);
+
+        GetComponent<Rigidbody>().AddForce(transform.forward * bulletSpeed, ForceMode.Impulse);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Player")
+        {
+            print("hit " + other.name + "!");
+            Destroy(gameObject);
+        }
+    }
+}
