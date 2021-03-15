846423a78018e235eb2a360dcfe55e29f36b0377
DepressedCookie <vipc.1224@live.ca>
2021-03-14 20:40:30 -0400

    misc

--- Assets/3_Scripts/Other/S_DayNight.cs
+++ Assets/3_Scripts/Other/S_DayNight.cs
@@ -22,21 +22,20 @@ public class S_DayNight : MonoBehaviour
         if (!isNight)
         {
             transform.Rotate(new Vector3(speed * Time.deltaTime, 0, 0));
-            if (transform.position.x >= 180.0f)
+            if (transform.rotation.eulerAngles.x >= 180.0f)
             {
                 isNight = true;
                 lightComp.intensity = 0;
                 StartCoroutine(ResetSun());
             }
         }
+
     }
 
     IEnumerator ResetSun()
     {
         yield return new WaitForSeconds(nightRest);
-        Quaternion rot = transform.rotation;
-        rot.x = 0;
-        transform.rotation = rot;
+        transform.rotation = Quaternion.Euler(new Vector3(0, -49.95f, 0));
         lightComp.intensity = OriLux;
         isNight = false;
     }
