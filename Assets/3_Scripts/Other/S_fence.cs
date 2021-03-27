ec48ccdf7d9112e12b7ddae2fd20188212b6c766
VergilZh <z1205067147@gmail.com>
2021-03-27 00:54:02 -0400

    Add base defense wall

---
+++ Assets/3_Scripts/Other/S_fence.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_fence : MonoBehaviour
+{
+    public float fenceHealth;
+    public bool fenceAlive = true;
+    public float attackDelay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fenceHealth = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fenceHealth <= 0)
+        {
+            Destroy(gameObject);
+            fenceAlive = false;
+        }
+    }
+
+    IEnumerator GetDamage()
+    {
+        while (fenceAlive == true)
+        {
+            fenceHealth -= 1;
+            yield return new WaitForSeconds(2);
+        }
+    }
+
+    //public void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.tag == "Enemy")
+    //    {
+    //        other.GetComponent<Enemy>().DoAttack();
+    //        StartCoroutine((IEnumerator)WaitForSeconds());
+    //        fenceHealth = (fenceHealth - 1);
+    //    }
+    //}
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            other.GetComponent<Enemy>().DoAttack();
+            StartCoroutine(GetDamage());
+        }
+    }
+
+
+}
