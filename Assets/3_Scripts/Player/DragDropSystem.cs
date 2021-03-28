6d6fa7f5ea2e7939613958cba0df9b99b4269531
DepressedCookie <vipc.1224@live.ca>
2021-03-28 19:39:22 -0400

    misc

--- Assets/3_Scripts/Player/DragDropSystem.cs
+++ Assets/3_Scripts/Player/DragDropSystem.cs
@@ -19,19 +19,11 @@ public class DragDropSystem : MonoBehaviour
 
             if (Physics.Raycast(ray, out hit, Mathf.Infinity, 1 << LayerMask.NameToLayer("Terrain")))
             {
-                if (!InPlacement)
-                {
-                    InPlacement = true;
-                    GameObject Item = SpawnItems[Random.Range(0, SpawnItems.Count)];
-                    PlacementItem = Instantiate(Item, HitPosition, new Quaternion());
-                    PlacementItem.transform.Rotate(Vector3.up, Random.Range(0, 360));
-                    Quests.placedTurrets += 1;
-                }
                 HitPosition = hit.point;
                 PlacementItem.transform.position = HitPosition;
             }
 
-            if(Input.GetMouseButtonUp(0))
+            if (Input.GetMouseButtonUp(0))
             {
                 PlacementItem = null;
             }
